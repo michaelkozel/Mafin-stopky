579ab5c1cc86dd15c1981e7523ca45d1b82142b9
Michael Kozel <michael.kozel1@gmail.com>
2017-10-10 18:36:17 +0200

    Initial commit

---
+++ app/src/main/java/kozliksoft/mafin_stopky/DisplayCounting.java
@@ -0,0 +1,90 @@
+package kozliksoft.mafin_stopky;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DisplayCounting extends AppCompatActivity {
+
+    protected int minutes;
+    protected TextView txbTime;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_counting);
+
+        Intent intent = getIntent();
+        txbTime = (TextView) findViewById(R.id.txbTime);
+        minutes = intent.getIntExtra(MainActivity.MINUTES, 0);
+        //txbTime.setText(this.minutes);
+
+        try {
+            startCounting();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    protected void startCounting() throws ParseException {
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        String start = dateFormat.format(date);
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Date date1 = format.parse(start);
+        long difference = minutes * 60000 - 0;
+
+
+        CountDownTimer timer = new CountDownTimer(difference, 500) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+
+                long second = (millisUntilFinished / 1000) % 60;
+                long minute = (millisUntilFinished / (1000 * 60)) % 60;
+                long hour = (millisUntilFinished / (1000 * 60 * 60)) % 24;
+
+
+                String time;
+
+                txbTime.setTextSize(150);
+                if (minute < 1) {
+                    {
+                        time = "0:" + Long.toString(second);
+                    }
+                    txbTime.setText(time);
+                } else {
+                    time = Long.toString((millisUntilFinished / 60000) % 60);
+                }
+                txbTime.setText(time);
+
+
+            }
+
+            @Override
+            public void onFinish() {
+                txbTime.setTextColor(Color.parseColor("#ff0000"));
+                txbTime.setText("KONEC");
+                txbTime.setTextSize(150);
+
+            }
+        };
+        timer.start();
+    }
+}
---
+++ app/src/main/java/kozliksoft/mafin_stopky/MainActivity.java
@@ -0,0 +1,48 @@
+package kozliksoft.mafin_stopky;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public final static String HOURS = "svandeliksoftware.countdown.HOURS";
+    public final static String MINUTES = "svandeliksoftware.countdown.MINUTES";
+    EditText minutesAdd ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         minutesAdd = (EditText)findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button);
+        button.setText("Start");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prepareCounting(v);
+            }
+        });
+    }
+
+    /**
+     * Called when the user clicks the Send button
+     */
+    public void prepareCounting(View view) {
+        Intent intent = new Intent(this, DisplayCounting.class);
+int minutes = Integer.parseInt(minutesAdd.getText().toString());
+
+
+        //button.setText(timePicker.getCurrentMinute().toString());
+
+        intent.putExtra(MINUTES, minutes);
+        startActivity(intent);
+    }
+
+}
