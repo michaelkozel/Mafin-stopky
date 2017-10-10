96759b90251c3a98f29cdb4eeefd58adcdc0f8b3
Michael Kozel <michael.kozel1@gmail.com>
2017-10-10 19:20:31 +0200

    Přidáno firebase

---
+++ app/src/main/java/kozliksoft/mafin_stopky/FirebaseIDService.java
@@ -0,0 +1,35 @@
+package kozliksoft.mafin_stopky;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FirebaseIDService extends FirebaseInstanceIdService {
+    private static final String TAG = "FirebaseIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        Log.d("TOKEN",token);
+        // Write a message to the database
+
+
+    }
+}
