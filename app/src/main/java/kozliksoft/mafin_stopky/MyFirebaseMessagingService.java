3e11a451157b1926b85e51b756b9c6c0bdad976e
Michael Kozel <michael.kozel1@gmail.com>
2017-10-10 23:49:08 +0200

    Funguje odpočet při dostání času přes notifikaci

--- app/src/main/java/kozliksoft/mafin_stopky/MyFirebaseMessagingService.java
+++ app/src/main/java/kozliksoft/mafin_stopky/MyFirebaseMessagingService.java
@@ -1,48 +1,64 @@
 package kozliksoft.mafin_stopky;
 
+import android.app.Activity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Toast;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
+    private Context appContext;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
-  //      Log.d(TAG, "From: " + remoteMessage.getFrom());
+        //      Log.d(TAG, "From: " + remoteMessage.getFrom());
 //        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
 
-try{
-    int minutes = Integer.parseInt(remoteMessage.getData().get("minutes"));
-    Intent i = new Intent(this,DisplayCounting.class);
+        try {
+            appContext = getBaseContext();//Get the context here
+            int minutes = Integer.parseInt(remoteMessage.getData().get("minutes"));
+            Intent i = new Intent(this, DisplayCounting.class);
 
-    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    i.putExtra(MainActivity.MINUTES, minutes);
-    startActivity(i);
-}
-       catch (Exception e)
-       {
-
-         Log.d("TAG","Chyba");
-
-       }
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.putExtra(MainActivity.MINUTES, minutes);
+            startActivity(i);
+        } catch (Exception e) {
+            showToast();
+            Log.d("TAG", "Chyba");
 
 
-
+        }
 
 
     }
 
+    //Use this method to show toast
+    void showToast() {
+        if (null != appContext) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(appContext, "Chyba", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
 
 }
